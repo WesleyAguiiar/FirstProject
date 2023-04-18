54dd3c96459be327c504d9512067e91ae836b7aa
Wesley Aguiar <aguiar.softeng@gmail.com>
2023-04-18 20:48:20 -0300

    Adição do JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+function foco() {
+    let focus = document.getElementById("ex1")
+    focus.setAttribute("class", "gray")
+}
+
+function unFoco() {
+    let focus = document.getElementById("ex1")
+    focus.setAttribute("class", "normal")
+}
