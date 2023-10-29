50a210c5e1ed3977962ed5be9d1e5693657d202f
Wesley Aguiar <125822856+WesleyAguiiar@users.noreply.github.com>
2023-10-29 19:00:38 -0300

    Conclusão do trabalho de fundamentos da programação WEB

--- script.js
+++
@@ -1,9 +0,0 @@
-function foco() {
-    let focus = document.getElementById("ex1")
-    focus.setAttribute("class", "gray")
-}
-
-function unFoco() {
-    let focus = document.getElementById("ex1")
-    focus.setAttribute("class", "normal")
-}
